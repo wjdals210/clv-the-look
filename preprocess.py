0c60c10a883ce1bc0229f1f71420539d57498695
Ian Carrier <ianpcarrier@gmail.com>
2024-03-04 14:47:43 +0100

    Added library

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,11 @@
-def data_cleaning(order_units_df):
+import pandas as pd
+
+def data_cleaning(order_items_df):
     '''
     This function takes the order_units dataframe, cleans the data and returns the cleaned dataframe
     '''
 
     # Convert the created_at column from object to datetime
-    order_units_df['created_at'] = pd.to_datetime(order_units_df.created_at,format='mixed')
+    order_items_df['created_at'] = pd.to_datetime(order_items_df.created_at,format='mixed')
 
     return df
