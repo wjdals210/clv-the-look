2e27de86bbfaca36b2ed8362198d92860a80acde
Ian Carrier <ianpcarrier@gmail.com>
2024-03-04 14:51:29 +0100

    Corrected some errors in the preprocess.py file

--- preprocess.py
+++ preprocess.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 def data_cleaning(order_items_df):
     '''
-    This function takes the order_units dataframe, cleans the data and returns the cleaned dataframe
+    This function takes the order_items dataframe, cleans the data and returns the cleaned dataframe
     '''
 
     # Convert the created_at column from object to datetime
     order_items_df['created_at'] = pd.to_datetime(order_items_df.created_at,format='mixed')
 
-    return df
+    return order_items_df
