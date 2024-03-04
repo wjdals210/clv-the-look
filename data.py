6655b3bd28fa7645490a415f2e82e2fcef54fd3d
Ian Carrier <ianpcarrier@gmail.com>
2024-03-04 17:28:50 +0100

    Updated data.py script to aggregate and merge the orders data

--- data.py
+++ data.py
@@ -1,14 +1,19 @@
 import pandas as pd
 from lifetimes.utils import summary_data_from_transaction_data
 
-def data_cleaning(order_items_df):
+def data_cleaning(order_items_df, orders_df):
     '''
-    This function takes the order_items dataframe, cleans the data and returns the cleaned dataframe
+    This function takes the order_items and the orders tables, aggregates the order_items table,
+    merges the tables, cleans the datatypes and returns the cleaned dataframe
     '''
     # Convert the created_at column from object to datetime
-    order_items_df['created_at'] = pd.to_datetime(order_items_df.created_at,format='mixed')
 
-    return order_items_df
+    order_sales = order_items_df[['order_id','sale_price']].groupby(by='order_id').sum('sale_price').reset_index()
+    order_sales = order_sales.merge(orders_df[['order_id','user_id','created_at']]).set_index('order_id')
+
+    order_sales['created_at'] = pd.to_datetime(order_sales.created_at,format='mixed')
+
+    return order_sales
 
 def rfm(cleaned_order_items_df):
     '''
