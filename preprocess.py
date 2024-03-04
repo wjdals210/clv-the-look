b384c0665ad6bc55edebdd1132b394113521ad9b
Ian Carrier <ianpcarrier@gmail.com>
2024-03-04 14:56:44 +0100

    Added the rfm function to the preprocess.py file

--- preprocess.py
+++ preprocess.py
@@ -9,3 +9,12 @@ def data_cleaning(order_items_df):
     order_items_df['created_at'] = pd.to_datetime(order_items_df.created_at,format='mixed')
 
     return order_items_df
+
+def rfm(cleaned_order_units_df):
+    '''
+    This function takes the cleaned order_items dataframe and outputs the Recency, Frequency and Tenure dataframe
+    '''
+    return summary_data_from_transaction_data(transactions = df,
+                                              customer_id_col = 'user_id',
+                                              datetime_col = 'created_at',
+                                              monetary_value_col = 'sale_price')
