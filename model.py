ebb3ff60ce718c9be2501e032c4951e3be346d10
Ian Carrier <ianpcarrier@gmail.com>
2024-03-04 20:12:09 +0100

    Created new files and worked on data script

--- model.py
+++ model.py
@@ -6,8 +6,8 @@ def train_model(cleaned_rfm_train_df):
     '''
     pen_coef_ = 0.001
     model = BetaGeoFitter(penalizer_coef=pen_coef_)
-    model.fit(cleaned_rfm_train_df['frequency_train'],
-              cleaned_rfm_train_df['recency_train'],
-              cleaned_rfm_train_df['T_train'])
+    model.fit(cleaned_rfm_train_df['frequency_cal'],
+              cleaned_rfm_train_df['recency_cal'],
+              cleaned_rfm_train_df['T_cal'])
     print("✅ Model initialized")
     return model
