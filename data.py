d6aae0abca493823d822cb8e6f62de2265cb72e2
Ian Carrier <ianpcarrier@gmail.com>
2024-03-05 16:00:40 +0100

    Updated comments in the script

--- data.py
+++ data.py
@@ -6,11 +6,12 @@ def data_cleaning(order_items_df, orders_df):
     This function takes the order_items and the orders tables, aggregates the order_items table,
     merges the tables, cleans the datatypes and returns the cleaned dataframe
     '''
-    # Convert the created_at column from object to datetime
 
+    # Agreegate the sale_price by order_id and merge the orders table
     order_sales = order_items_df[['order_id','sale_price']].groupby(by='order_id').sum('sale_price').reset_index()
     cleaned_order_sales = order_sales.merge(orders_df[['order_id','user_id','created_at']]).set_index('order_id')
 
+    # Convert the created_at column from 'object' to datetime64
     cleaned_order_sales['created_at'] = pd.to_datetime(cleaned_order_sales['created_at'],
                                                        format='mixed').dt.tz_localize(None)
 
--- model.py
+++ model.py
@@ -1,32 +1,29 @@
 from lifetimes import BetaGeoFitter
 from lifetimes import GammaGammaFitter
 
-def train_bgmodel(cleaned_rfm_train_df):
+def train_bgmodel(cleaned_separated_rfm_df):
     '''
-    Function to fit the Beta-Geometric model on the cleaned, training dataset (which has been converted to rfm)
+    Function to fit the Beta-Geometric model on the cleaned, calibration/holdout dataset
+    (which has been converted to rfm)
     '''
     pen_coef_ = 0.001
     bg_model = BetaGeoFitter(penalizer_coef=pen_coef_)
-    bg_model.fit(cleaned_rfm_train_df['frequency_cal'],
-              cleaned_rfm_train_df['recency_cal'],
-              cleaned_rfm_train_df['T_cal'])
+    bg_model.fit(cleaned_separated_rfm_df['frequency_cal'],
+                 cleaned_separated_rfm_df['recency_cal'],
+                 cleaned_separated_rfm_df['T_cal'])
+
     print("✅ ß-Geo Model trained")
+
     return bg_model
 
 def train_ggmodel(filtered_rfm_df):
-    gg_model = GammaGammaFitter()
-    gg_model.fit(filtered_rfm_df['frequency'], filtered_rfm_df['monetary_value'])
-    print("✅ γγ Model trained")
-    return gg_model
+    '''
+    Function to fit the gamma-gamma model to the
+    '''
 
-def clv(gg_model,bg_model,rfm_df):
-    '''
-    Function to output the 'customer lifetime value' for the next 3 months given an input rfm dataframe
-    '''
-    gg_model.customer_lifetime_value(bg_model,
-                                    rfm_df['frequency'],
-                                    rfm_df['recency'],
-                                    rfm_df['T'],
-                                    rfm_df['monetary_value'],
-                                    time = 3,# In months
-                                    )
+    gg_model = GammaGammaFitter()
+    gg_model.fit(filtered_rfm_df['frequency_cal'], filtered_rfm_df['monetary_value_cal'])
+
+    print("✅ γγ Model trained")
+
+    return gg_model
